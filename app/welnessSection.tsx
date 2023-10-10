6f0d79e5afea87d2f77e1ffb8736e33c85c7a4c5
Yorick De Jong <yorickdejong@gmail.com>
2023-10-10 08:31:08 +0200

    improved usability more

--- app/welnessSection.tsx
+++ app/welnessSection.tsx
@@ -19,7 +19,7 @@ export default function WellnessSection() {
         offset: ['start end', 'end start']
     });
     
-    const smoothScrollYProgress = useSpring(scrollYProgress, { stiffness: 85, damping: 30 });
+    const smoothScrollYProgress = useSpring(scrollYProgress, { stiffness: 85, damping: 20 });
 
 
 
@@ -55,7 +55,7 @@ export default function WellnessSection() {
         whileInView={"onscreen"}
         viewport={{ once: true, amount: 0.3 }}
         transition={{ staggerChildren: 0.1 }}
-        style = {{translateY: svgheight * 200 }}
+        style = {{translateY: svgheight * 100 }}
         className="my-12 mt-24">
             <CurveSVG 
             height={svgheight}
@@ -65,7 +65,7 @@ export default function WellnessSection() {
             ref={containerRef}
             style={{ backgroundColor: color,  }}
             >
-                <MotionWrapper style= {{translateY: -svgheight * 200}} className="container grid grid-cols-1 md:grid-cols-2 gap-20 py-24">
+                <MotionWrapper style= {{translateY: -svgheight * 150}} className="container grid grid-cols-1 md:grid-cols-2 gap-20 py-24">
                     <MotionWrapper key={1} div variants={fadeUp} className="mt-12">
                         <Images.diensten_hero className = 'diensten-animate'/>
                     </MotionWrapper>
